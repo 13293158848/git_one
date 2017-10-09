f1fad7da5f32c5a538eb8053bfe63ae638323e32
root <2388477610@qq.com>
2017-10-09 17:17:21 +0800

    测试1

---
+++ onetest/src/main/java/com/web/controller/TwoController.java
@@ -0,0 +1,14 @@
+package com.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TwoController {
+
+	
+	public String testtwo(HttpServletRequest req){
+		return null;
+	}
+}
